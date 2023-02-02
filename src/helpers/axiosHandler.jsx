202581635dfd5e2b40418d980c084b3d58f76021
Rosita <esorthe7@gmail.com>
2023-02-02 20:21:16 +0530

    delete initial frontend

--- src/helpers/axiosHandler.jsx
+++
@@ -1,6 +0,0 @@
-import { toast } from "react-toastify";
-
-export default function axiosHandler(error) {
-    toast.error(error);
-    return;
-}
--- src/helpers/errorHandler.js
+++
@@ -1,5 +0,0 @@
-import { toast } from 'material-react-toastify';
-
-export default function axiosHandler(error) {
-    toast.error(error);
-}
