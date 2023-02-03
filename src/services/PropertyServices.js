307450f1037bcfe20246469f7c25d1cd238e2fb4
harsh0989 <harshmangukiya1506@gmail.com>
2023-02-03 10:06:54 +0530

    Changes

--- src/services/PropertyServices.js
+++ src/services/PropertyServices.js
@@ -8,7 +8,15 @@ const getOneProperty=(id)=>{
     return httpCommon.get(`/property/single/${id}`)
 }
 
+const getOnePropertywithCompatibility=(id,token)=>{
+    return httpCommon.get(`/property/single/compatibility/${id}`,{
+        "headers": {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+}
 export default {
     getAllProperties,
-    getOneProperty
+    getOneProperty,
+    getOnePropertywithCompatibility
 }
